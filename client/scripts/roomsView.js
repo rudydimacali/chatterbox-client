9ab3ff3921ff18b6fb53474650c5df147dfda751
Hack Reactor Student <student@hackreactor.com>
2018-11-16 17:30:17 -0800

    Completed adding room functionality.

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -2,8 +2,10 @@ var RoomsView = {
 
   $button: $('#rooms button'),
   $select: $('#rooms select'),
+  $addRoom: $('#addRoom'),
 
   initialize: function() {
+    RoomsView.$addRoom.on('click', RoomsView.handleAddRoom);
   },
 
   render: function() {
@@ -14,6 +16,13 @@ var RoomsView = {
     var formatRoom = _.template("<div><b><%- room %></b></div>");
     var formattedRoom = formatRoom({room: this.button});
     this.$select.append(formattedRoom);
+  },
+  
+  handleAddRoom: function(event) {
+    var roomName = document.createElement("option");
+    roomName.text = $("#addRoomName").val();
+    Rooms.push(roomName.text);
+    document.getElementById("roomSelector").add(roomName);
   }
 
 };
